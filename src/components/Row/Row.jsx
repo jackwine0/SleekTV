27abd1e58ceeb41c1f4b2c15bff4ab6fd42b4ca5
jackwine0 <samakande98@gmail.com>
2024-08-07 01:46:07 +0100

    v

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
-import axios from 'axios'; // Import axios for fetching data
+import axios from 'axios';
 import './Row.css'; // Import your CSS file for styling
+import { FaHeart, FaRegHeart } from 'react-icons/fa'; // Import heart icons
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false); // State for the like button
   const rowRef = useRef(null); // Create a ref for the row container
 
   useEffect(() => {
@@ -31,10 +33,20 @@ const Row = ({ title, fetchURL }) => {
     }
   };
 
+  const saveShow = () => {
+    setLike(!like); // Toggle the like state
+  };
+
   return (
     <div className="row">
       <h2 className="row-title">{title}</h2>
       <div className="row-slider-container">
+        <button className="nav-button nav-button-left" onClick={scrollLeft}>
+          <img
+            src="https://res.cloudinary.com/duicyr28v/image/upload/v1722981365/icons8-back-100_caagax.png"
+            alt="Back"
+          />
+        </button>
         <div className="row-slider" ref={rowRef}>
           {movies.map((item, id) => (
             <div className="row-item" key={id}>
@@ -47,16 +59,17 @@ const Row = ({ title, fetchURL }) => {
                 <div className="overlay">
                   <p className="overlay-text">{item.title}</p>
                 </div>
+                <div className="like-button" onClick={saveShow}>
+                  {like ? (
+                    <FaHeart className='like-icon' />
+                  ) : (
+                    <FaRegHeart className='like-icon' />
+                  )}
+                </div>
               </div>
             </div>
           ))}
         </div>
-        <button className="nav-button nav-button-left" onClick={scrollLeft}>
-          <img
-            src="https://res.cloudinary.com/duicyr28v/image/upload/v1722981365/icons8-back-100_caagax.png"
-            alt="Back"
-          />
-        </button>
         <button className="nav-button nav-button-right" onClick={scrollRight}>
           <img
             src="https://res.cloudinary.com/duicyr28v/image/upload/v1722981365/icons8-forward-100_qus0nq.png"
@@ -65,7 +78,7 @@ const Row = ({ title, fetchURL }) => {
         </button>
       </div>
     </div>
-  );https://res.cloudinary.com/duicyr28v/image/upload/v1722985816/icons8-heart-100_fix07h.png
+  );
 };
 
 export default Row;
